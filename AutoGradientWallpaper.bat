d67aa874a0f190a40151ff5f62e2451081662122
Sarath Sajan <sarathsajan1998@gmail.com>
2023-04-18 23:03:36 +0530

    Used PyInstaller to create standalone app

--- AutoGradientWallpaper.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-"D:\projects\python\_virtualenvironment\Scripts\python.exe" "D:\projects\python\auto-gradient-wallpaper\app.py"
-@REM in the above line its "absolute path to python in virtual env" "absolute path to our python script"
